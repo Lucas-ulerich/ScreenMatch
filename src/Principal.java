a9dba1e9a941511baeda87f3dfc1f21b1896377d
Lucas-ulerich-TOBrasil <lucas.ulerichsouza@gmail.com>
2024-09-03 18:10:50 -0300

    Adicionando novas funções

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import javax.swing.plaf.basic.BasicSplitPaneUI;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMunitos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
+
+
+    }
+}
